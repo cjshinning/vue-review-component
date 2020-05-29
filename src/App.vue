832cbba20e4f7e128dcae7018ae75523d0cf4503
cjshinning <784319417@qq.com>
2020-05-29 17:28:56 +0800

    Vue组件基础-通过组件向子组件传递数据

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
     <div id="app">
+        <!-- <button-counter></button-counter>
         <button-counter></button-counter>
-        <button-counter></button-counter>
-        <button-counter></button-counter>
+        <button-counter></button-counter> -->
+
+        <!-- <blog-post title="My Journet with Vue"></blog-post>
+        <blog-post title="Blogging with Vue"></blog-post>
+        <blog-post title="Why Vue is so fun"></blog-post> -->
+
+        <blog-post
+            v-for="post in posts"
+            v-bind:key="post.id"
+            v-bind:title="post.title"
+        ></blog-post>
     </div>
 </template>
 <script>
-import ButtonCounter from './components/button-counter';
+// import ButtonCounter from './components/button-counter';
+import BlogPost from './components/blog-post';
 export default {
     name: 'App',
+    data(){
+        return {
+            posts: [
+                {
+                    id: 1,
+                    title: 'My Journet with Vue1'
+                },
+                {
+                    id: 2,
+                    title: 'My Journet with Vue2'
+                },
+                {
+                    id: 3,
+                    title: 'My Journet with Vue3'
+                }
+            ]
+        }
+    },
     components: {
-        ButtonCounter
+        // ButtonCounter
+        BlogPost
     }
 }
 </script>
---
+++ src/components/blog-post.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>{{title}}</h3>
+</template>
+<script>
+export default {
+    props: ['title']
+}
+// 通过props向子元素传递数据
+</script>
