f5f3e2ba50e05c986ea2b5c925eaa79e1e3609ff
cjshinning <784319417@qq.com>
2020-06-01 17:36:50 +0800

    渲染函数-模板方法

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <button
+        <!-- <button
             v-for="tab in tabs"
             v-bind:key="tab"
             v-bind:class="['tab-button', {active: currentTab === tab}]"
@@ -10,27 +10,44 @@
         </button>
         <keep-alive>
             <component v-bind:is="currentTabComponent"></component>
-        </keep-alive>
+        </keep-alive> -->
+        <!-- <button v-on:click="show=!show">Toggle</button> -->
+        <!-- <transition name="fade">
+            <p v-if="show">hello</p>
+        </transition> -->
+        <!-- 自定义 -->
+        <!-- <transition 
+            name="custom-classes-transition"
+            enter-active-class="enterClass"
+            leave-active-class="leaveClass"
+        >
+            <p v-if="show">hello</p>
+        </transition> -->
+        <anchored-heading :level='1'>Hello world</anchored-heading>
+        <anchored-heading :level='2'>Hello world</anchored-heading>
     </div>
 </template>
 <script>
 import './assets/css/style.css';
 import TabPosts from './components/TabPosts';
 import TabArchive from './components/TabArchive';
+import AnchoredHeading from './components/AnchoredHeading'
 export default {
     name: 'App',
     data(){
         return {
-            currentTab: 'Posts',
-            tabs: ['Posts', 'Archive']
+            // currentTab: 'Posts',
+            // tabs: ['Posts', 'Archive']
+            show: true
         }
     },
     methods: {
     },
     // 局部注册组件，在其子组件中不可复用
     components: {
-        TabPosts,
-        TabArchive
+        // TabPosts,
+        // TabArchive
+        AnchoredHeading
     },
     computed: {
         currentTabComponent: function(){
@@ -39,4 +56,20 @@ export default {
     }
 }
 </script>
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.enterClass{
+    transition: color red;
+}
+.leaveClass{
+    transform: scale(0);
+}
+</style>
+
 
